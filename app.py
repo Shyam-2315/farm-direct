c47484c3c815dcf6fbc68ed2e949a06482aefd6e
Shyam-2315 <snp2315@gmail.com>
2024-09-07 15:27:54 +0000

    Added Navbar
    Added Navbar

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello_world():
-  return "Hello Naren"
+def market_home():
+  return render_template('home.html')
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
-  
-  
